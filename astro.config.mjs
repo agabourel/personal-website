13ebac41581c11f703c2200c07f83964b63fb1d4
Ashlee Gabourel <107069129+agabourel@users.noreply.github.com>
2025-04-15 14:33:34 -0400

    "Fix base path for GitHub Pages"

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,3 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({});
+    output: 'static',
+    base: '/personal-website/', // your GitHub repo name here
+  });
